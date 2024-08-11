2028fb4badf84818e1c3c4bc7219ed02d8ea24a7
Arkopravo Ghosh <arkopravoghosh789@gmail.com>
2024-08-11 13:55:47 +0530

    11-08-24

---
+++ String/longest_palindromic_substring.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string &s, int i, int j)
+{
+    while (i <= j)
+    {
+        if (s[i] != s[j])
+        {
+            return false;
+        }
+        else
+        {
+            i++, j--;
+        }
+    }
+    return true;
+}
+string longestPalindrome(string s)
+{
+    string ans;
+    for (int i = 0; i < s.length(); i++)
+    {
+        for (int j = i; j < s.length(); j++)
+        {
+            if (isPalindrome(s, i, j))
+            {
+                string t = s.substr(i, j - i + 1);
+                ans = t.size() > ans.size() ? t : ans;
+            }
+        }
+    }
+    return ans;
+}
+
+int main() 
+{
+    string s = "abcbe";
+    string ans = longestPalindrome(s);
+
+    cout<<"Longest Palindromic Substring -> "<<ans<<endl;
+
+    return 0;
+}
